e05a90e8757fdfb8914ab383fb5bffb9e3f33ed0
Professional <new99kpicem@gmail.com>
2023-07-08 23:25:18 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import Impl.StringListImpl;
+import Interface.StringList;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        StringList m = new StringListImpl(5);
+        m.add("dfs");
+        System.out.println(Arrays.toString(m.toArray()));
+
+    }
+}
